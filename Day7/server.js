04f08a52baf312d863c8e3ca00c2f2600006ca78
Manas Singh <manasinghofficial@gmail.com>
2025-10-03 12:26:24 +0530

    socket.io basics

--- Day7/server.js
+++ Day7/server.js
@@ -1,6 +1,11 @@
 import app from "./src/app.js";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import dotenv from "dotenv";
+import { generateResponse } from "./src/service/ai.service.js";
+import { log } from "console";
+
+dotenv.config();
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -8,7 +13,17 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("ai-message", async (data) => {
+    console.log("Prompt received:", data.prompt);
+    const aiResponse = await generateResponse(data);
+    console.log("AI Response:", aiResponse);
+    socket.emit("ai-message-response", { aiResponse });
+  });
 });
 
 httpServer.listen(3000, () => {
--- Day7/src/app.js
+++ Day7/src/app.js
@@ -2,8 +2,6 @@ import express from "express"
 import { Server } from "socket.io"
 
 const app = express()
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-}) 
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 export default app
